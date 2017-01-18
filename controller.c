ed86a2268756a3745faf76a0742edfe847d203d0
exwgaet <exwgaet@gmail.com>
2017-01-18 15:48:43 +0100

    1st up for controller
    Idea + code ?

--- controller.c
+++ controller.c
@@ -5,6 +5,46 @@
  * Date : january 2017
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "controller.h"
+#include <signal.h>
 
-bla;
+static void handler(int signum) {
+    char msg[] = "SIGINT received! Aborted.\n";
+    write(STDOUT_FILENO, msg, strlen(msg));
+    _exit(EXIT_SUCCESS);
+}
+
+int main() {
+
+    sigset_t mask, maskold;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGQUIT);
+    sigqddset(&mask, SIGTSTP);
+    pthread_sigmask(SIG_SETMASK, &mask, &maskold);
+
+    int sig;
+    int x = 0;
+
+    do {
+        sigwait(&mask, &sig);
+        if (sig == SIGINT){
+            if(x < ){
+                wheel.x -> stop = true;
+            }
+        }else if (sig == SIGTSTP){
+            for (int i = 0; i < ; i++) {
+                wheel.i -> restart = true;
+            }
+        }
+            puts("Be strong, I whispered to my wifi signal!");
+    } while (sig != SIGQUIT);
+
+    for (int i = 0; i < ; i++) {
+        thread.i -> exit = true;
+    }
+
+    return EXIT_SUCCESS;
+}
