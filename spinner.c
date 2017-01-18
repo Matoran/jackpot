d94129f342bf004ebb56503047ca19201077f215
lekikou <lekikou@users.noreply.github.com>
2017-01-18 15:36:38 +0100

    Add Spinner

--- spinner.c
+++ spinner.c
@@ -2,4 +2,54 @@
 // Created by matoran on 1/18/17.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <memory.h>
+#include <zconf.h>
 #include "spinner.h"
+
+
+
+void createThreads(uint numberWorkers){
+    pthread_t threads[numberWorkers];
+    paramsSpinnerSt paramsThread[numberWorkers];
+    bool quit = false;
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex,NULL);
+
+    for (uint i = 0; i < numberWorkers; i++) {
+        paramsThread[i].idThread = i+1;
+        paramsThread[i].numberThreads = numberWorkers;
+        paramsThread[i].pos = 0;
+        paramsThread[i].value = ALPHABET[0];
+        paramsThread[i].quit = &quit;
+        paramsThread[i].mutex = &mutex;
+        int code = pthread_create(&threads[i], NULL, spinner, &paramsThread[i]);
+        if (code != 0) {
+            fprintf(stderr, "pthread_create failed!\n");
+            exit(42);
+        }
+    }
+}
+
+void *spinner(void *paramsThread){
+    paramsSpinnerSt *params = (paramsSpinnerSt *) paramsThread;
+    struct timespec start, finish;
+    double microSecondToWait = 120 / params->idThread;
+
+    while(!params->quit){
+        if(params->pos < strlen(ALPHABET)) {
+            params->pos++;
+        }else{
+            params->pos = 0;
+        }
+        clock_gettime(CLOCK_REALTIME, &finish);
+        double elapsed = (finish.tv_sec - start.tv_sec) * 1000000;
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000.0;
+        if (microSecondToWait - elapsed > 0) {
+            usleep(microSecondToWait - elapsed);
+        }
+        params->value = ALPHABET[params->pos];
+    }
+}
--- spinner.h
+++ spinner.h
@@ -5,4 +5,22 @@
 #ifndef JACKPOT_SPINNER_H
 #define JACKPOT_SPINNER_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ALPHABET "123456789"
+
+typedef struct paramsSpinnerSt {
+    uint numberThreads;
+    uint idThread;
+    uint pos;
+    char value;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *cond;
+    bool *quit;
+} paramsSpinnerSt;
+
+void *spinner(void *paramsThread);
+void createThreads(uint numberWorkers);
+
 #endif //JACKPOT_SPINNER_H
